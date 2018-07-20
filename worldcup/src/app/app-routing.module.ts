32ec5efbc51e49a32274af6d10bea4f676fc0fdf
Alison Gale <agale753@gmail.com>
2018-07-19 19:38:31 -0700

    Add simple routing

--- worldcup/src/app/app-routing.module.ts
+++ worldcup/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ItineraryComponent } from './itinerary/itinerary.component';
+import { ScheduleComponent } from './schedule/schedule.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'schedule', component: ScheduleComponent },
+    { path: 'itinerary', component: ItineraryComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full'},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
