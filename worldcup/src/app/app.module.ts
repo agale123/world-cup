2fb3291be91c4c47083ff85f38644aad79fec102
Alison Gale <agale753@gmail.com>
2018-07-21 08:24:54 -0700

    Adding basic navigation between pages

--- worldcup/src/app/app.module.ts
+++ worldcup/src/app/app.module.ts
@@ -1,5 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatTabsModule
+} from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,17 +16,23 @@ import { ScheduleComponent } from './schedule/schedule.component';
 import { ItineraryComponent } from './itinerary/itinerary.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    ScheduleComponent,
-    ItineraryComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        ScheduleComponent,
+        ItineraryComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        MatButtonModule,
+        MatMenuModule,
+        MatTabsModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatCardModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
