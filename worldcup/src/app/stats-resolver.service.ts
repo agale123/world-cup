93897e0a6ee569d10b1f383167e3f71917f93753
Alison Gale <agale753@gmail.com>
2018-12-01 11:35:20 -0800

    Add all qualified teams and update map to handle England and Scotland

--- worldcup/src/app/stats-resolver.service.ts
+++ worldcup/src/app/stats-resolver.service.ts
@@ -13,6 +13,18 @@ export class StatsResolver implements Resolve<Observable<Object>> {
 
     resolve() {
         return this.http.get('assets/stats.json').pipe(
-            map((response) => response['data']));
+            map((response) => response['data']),
+            map(data => {
+                return data.map(row => {
+                    if (row[0] === 'England') {
+                        return [
+                            {v: row[0], f: 'England/Scotland'},
+                            {v: row[1], f: '5/1'},
+                            {v: row[2], f: '0/0'},
+                        ];
+                    }
+                    return [{v: row[0]}, row[1], row[2]];
+                });
+            }));
     }
 }
